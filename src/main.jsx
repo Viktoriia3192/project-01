a3bed385878758e9fe33bb00109cfa1b2f97ab03
Viktoriia3192 <vikysja9231@gmail.com>
2024-04-10 21:32:11 +0100

    com4

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/project-01">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
