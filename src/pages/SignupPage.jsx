c074cab945bd3d2ca8ab09229fd2cb47af8271e3
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2024-04-12 17:47:48 -0500

    signup draft

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,5 +1,53 @@
+import { useFormik } from 'formik';
+
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  // setting initial values for form fields
+  const initialValues = {
+    firstName: '',
+    lastName: '',
+    email: '',
+  };
+
+  // defining handleSubmit function for form submission
+  const handleSubmit = (values) => {
+    alert(JSON.stringify(values, null, 2));
+  };
+
+  // using useFormik hook to handle form state and validation
+  const formik = useFormik({
+    initialValues,
+    onSubmit: handleSubmit,
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default SignupPage;
