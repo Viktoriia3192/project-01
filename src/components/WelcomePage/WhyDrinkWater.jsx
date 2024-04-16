99008cfea1fa766e522740268f6e5d8c155753f9
DenysTkachov <D.Tkachev@ukr.net>
2024-04-16 16:20:05 +0200

    add icon

--- src/components/WelcomePage/WhyDrinkWater.jsx
+++ src/components/WelcomePage/WhyDrinkWater.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+
 import css from './WhyDrinkWater.module.css'; 
-import ellipse from '../../icons/ellipse.svg';
+import ellipseSymbol from '../../icons/ellipse.svg';
 
 const WhyDrinkWater = () => {
   return (
     <div className={css.whyWaterContainer}>
       <h3 className={css.whyWaterContainerH3}>Why drink water</h3>
       <ul className={css.whyWaterContainerUl}>
-        <li><img src={ellipse} alt="Ellipse" className={css.whyWaterContainerSvg} />Supply of nutrients to all organs</li>
-        <li><img src={ellipse} alt="Ellipse" className={css.whyWaterContainerSvg} />Providing oxygen to the lungs</li>
-        <li><img src={ellipse} alt="Ellipse" className={css.whyWaterContainerSvg} />Maintaining the work of the heart</li>
-        <li><img src={ellipse} alt="Ellipse" className={css.whyWaterContainerSvg} />Release of processed substances</li>
-        <li><img src={ellipse} alt="Ellipse" className={css.whyWaterContainerSvg} />Ensuring the stability of the internal environment</li>
-        <li><img src={ellipse} alt="Ellipse" className={css.whyWaterContainerSvg} />Maintaining within the normal temperature</li>
-        <li><img src={ellipse} alt="Ellipse" className={css.whyWaterContainerSvg} />Maintaining an immune system capable of resisting disease</li>
+        <li><svg className={css.whyWaterContainerSvg}>
+            <use xlinkHref={`${ellipseSymbol}#ellipse`} />
+          </svg>Supply of nutrients to all organs</li>
+        <li><svg className={css.whyWaterContainerSvg}>
+            <use xlinkHref={`${ellipseSymbol}#ellipse`} />
+          </svg>Providing oxygen to the lungs</li>
+        <li><svg className={css.whyWaterContainerSvg}>
+            <use xlinkHref={`${ellipseSymbol}#ellipse`} />
+          </svg>Maintaining the work of the heart</li>
+        <li><svg className={css.whyWaterContainerSvg}>
+            <use xlinkHref={`${ellipseSymbol}#ellipse`} />
+          </svg>Release of processed substances</li>
+        <li><svg className={css.whyWaterContainerSvg}>
+            <use xlinkHref={`${ellipseSymbol}#ellipse`} />
+          </svg>Ensuring the stability of the internal environment</li>
+        <li><svg className={css.whyWaterContainerSvg}>
+            <use xlinkHref={`${ellipseSymbol}#ellipse`} />
+          </svg>Maintaining within the normal temperature</li>
+        <li><svg className={css.whyWaterContainerSvg}>
+            <use xlinkHref={`${ellipseSymbol}#ellipse`} />
+          </svg>Maintaining an immune system capable of resisting disease</li>
       </ul>
     </div>
   );
