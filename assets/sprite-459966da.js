e3c5bde01703123ecee0127bdfec98927b0d8a5a
Viktoriia3192 <127699121+Viktoriia3192@users.noreply.github.com>
2024-04-17 17:17:42 +0000

    Deploying to gh-pages from @ Viktoriia3192/project-01@978f9026c92323278c04d7fecbf3ac9a7e1590b3 🚀

---
+++ assets/sprite-459966da.js
@@ -0,0 +1,1 @@
+import"./index-6f42894d.js";const t="/project-01/assets/sprite-8cc6e219.svg";export{t as s};
--- assets/sprite-d280cb18.js
+++
@@ -1,1 +0,0 @@
-import"./index-be1c386c.js";const e="/project-01/assets/sprite-b29f4e2e.svg";export{e as s};
