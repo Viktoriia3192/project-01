2a134661f8a3abb0866b3b1cb9c65ad463be2f33
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2024-04-17 19:41:45 +0300

    todayWaterList callbacks, html, css

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -1,94 +1,114 @@
-import { useSelector } from 'react-redux';
-import s from './TodayWaterList.module.css';
-import sprite from '../../images/sprite.svg';
+import { useDispatch } from 'react-redux';
 
-import { selectTodayWater } from '../../redux/waterData/waterSelectors';
+import sprite from '../../images/sprite.svg';
+import Modal from '../Modal/Modal';
+
+import s from './TodayWaterList.module.css';
+
+// import { selectTodayWater } from '../../redux/waterData/waterSelectors';
+import { useEffect, useState } from 'react';
+import { todayThunk } from '../../redux/waterData/waterOperations';
+import { todayWater } from './data';
 
 const TodayWaterList = () => {
-  const todayWaterPortions = useSelector(selectTodayWater);
+  // const todayWaterPortions = useSelector(selectTodayWater);
 
-  // const handleAddPortion = () => {
-  //   // toggleModal(<AddWaterModal />);
-  // };
-  // const handleEditPortion = (waterId) => {
-  //   // toggleModal(<DeleteEntryModal waterId={waterId} />);
-  // };
-  // const handleDeletePortion = (data) => {
-  //   // toggleModal(<EditAmountModal data={data} />);
+  // const [modalContent, setModalContent] = useState(null);
+  // const [modalData, setModalData] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(todayThunk());
+  }, [dispatch]);
+
+  // const openModal = (children, data) => {
+  //   setModalContent(children);
+  //   setModalIsOpen(true);
+  //   setModalData(data);
   // };
 
   return (
-    <div className={s.container}>
-      <h2 className={s.title}>Today</h2>
-      <div className={s.todayBox}>
-        <table className={s.waterTable}>
-          <tbody>
-            {todayWaterPortions
-              .toSorted(
-                (a, b) =>
-                  new Date(a.date).getTime() - new Date(b.date).getTime()
-              )
-              .map((data) => (
-                <tr key={data._id} className={s.tRow}>
-                  <td>
-                    <svg className={s.glassSvg}>
-                      <use href={`${sprite}#icon-glass`}></use>
-                    </svg>
-                  </td>
-                  <td>{data.waterVolume} ml</td>
-                  <td>
-                    {new Intl.DateTimeFormat('en', {
-                      hour: 'numeric',
-                      minute: 'numeric',
-                      hour12: true,
-                    }).format(new Date(data.date))}
-                  </td>
-                  <td>
-                    <button
-                      type="button"
-                      aria-label="Edit button"
-                      onClick={
-                        () => {}
-                        // handleEditPortion(data)
-                      }
-                    >
-                      <svg className={s.editSvg}>
-                        <use href={`${sprite}#icon-pencil`}></use>
+    <>
+      <div className={s.container}>
+        <h2 className={s.title}>Today</h2>
+        <div className={s.todayBox}>
+          <table className={s.waterTable}>
+            <tbody>
+              {todayWater.dosesWater
+                .toSorted(
+                  (a, b) =>
+                    new Date(a.date).getTime() - new Date(b.date).getTime()
+                )
+                .map((dose) => (
+                  <tr key={dose._id} className={s.tRow}>
+                    {/* <td className={s.doseInfo}> */}
+                    <td>
+                      <svg className={s.glassSvg}>
+                        <use href={`${sprite}#icon-glass`}></use>
                       </svg>
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      type="button"
-                      aria-label="Delete button"
-                      onClick={
-                        () => {}
-                        // handleDeletePortion(data._id)
-                      }
-                    >
-                      <svg className={s.editSvg}>
-                        <use href={`${sprite}#icon-trash`}></use>
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-        <button
-          type="submit"
-          onClick={
-            () => {}
-            // handleAddPortion
-          }
-        >
-          <svg className={s.plusSvg}>
-            <use href={`${sprite}#icon-plus-small`}></use>
-          </svg>
-          Add water
-        </button>
+                    </td>
+                    <td className={s.doseVolume}>{dose.waterVolume} ml</td>
+                    <td className={s.doseTime}>
+                      {new Intl.DateTimeFormat('en', {
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        hour12: true,
+                      }).format(new Date(dose.date)?.toJSON()?.split('T')[0])}
+                    </td>
+                    {/* </td> */}
+                    <td>
+                      <button
+                        type="button"
+                        aria-label="Edit button"
+                        className={s.editBtn}
+                        // onClick={() =>
+                        //   openModal(
+                        //     <EditWaterModal modalData={modalData} />,
+                        //     data
+                        //   )
+                        // }
+                      >
+                        <svg className={s.editSvg}>
+                          <use href={`${sprite}#icon-edit`}></use>
+                        </svg>
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        aria-label="Delete button"
+                        className={s.deleteBtn}
+                        // onClick={() =>
+                        //   openModal(<DeleteWaterModal ModalData />, data._id)
+                        // }
+                      >
+                        <svg className={s.deleteSvg}>
+                          <use href={`${sprite}#icon-delete`}></use>
+                        </svg>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <button
+            type="submit"
+            className={s.addBtn}
+            // onClick={() => openModal(<AddWaterModal />)}
+          >
+            <svg className={s.plusSvg}>
+              <use href={`${sprite}#icon-plus-small`}></use>
+            </svg>
+            Add water
+          </button>
+        </div>
       </div>
-    </div>
+      <Modal isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)}>
+        {/* {modalContent} */}
+      </Modal>
+    </>
   );
 };
 
---
+++ src/components/TodayWaterList/data.js
@@ -0,0 +1,14 @@
+export const todayWater = {
+  _id: '1',
+  date: '16-04-2024',
+  dosesWater: [
+    { waterVolume: 100, date: '16/04/2024', _id: '22' },
+    { waterVolume: 200, date: '16/04/2024', _id: '23' },
+    { waterVolume: 400, date: '16/04/2024', _id: '24' },
+    { waterVolume: 200, date: '16/04/2024', _id: '25' },
+    { waterVolume: 500, date: '16/04/2024', _id: '26' },
+  ],
+  totalWater: 1400,
+  waterRate: 2000,
+  owner: null,
+};
