845f3da74ce62a17bf5ef28dec9dd944b6ae8eb6
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2024-04-18 00:30:05 -0500

    signup test submit

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,42 +1,47 @@
-import { useFormik } from 'formik';
+import { useFormik, FormikContext } from 'formik';
 import s from '../SignUpForm/SignUpForm.module.css';
 import { signUpThunk } from '../../redux/auth/authOperations';
-import * as Yup from 'yup';
+import * as yup from 'yup';
 import { useDispatch } from 'react-redux';
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
   //const [showPassword, setShowPassword] = useState(false);
   //const { t } = useTranslation();
+  const SignUpSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email('Please enter a valid email')
+      .required('E-mail is required'),
+    password: yup
+      .string()
+      .min(8, 'Password must be 8 or more characters')
+      .max(64)
+      .required('Password is required'),
+    repeatPassword: yup
+      .string()
+      .oneOf([yup.ref('password'), null], 'The passwords do not match')
+      .required('Repeat password field is required'),
+  });
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const errors = await formik.validateForm();
+    if (Object.keys(errors).length === 0) {
+      formik.handleSubmit();
+    }
+  };
 
   const formik = useFormik({
     initialValues: {
       email: '',
       password: '',
-      repeatPassword: '',
+      confirmPassword: '',
     },
-    validationSchema: Yup.object({
-      email: Yup.string()
-        .email('Invalid email address')
-        .required('Email is required')
-        .matches(
-          /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-          'Invalid email address'
-        ),
-      password: Yup.string()
-        .required('Password is required')
-        .min(8, 'Password must be at least 8 characters')
-        .max(64, 'Password must be at most 64 characters'),
-      repeatPassword: Yup.string()
-        .oneOf([Yup.ref('password'), null], 'Passwords must match')
-        .required('Password is required'),
-    }),
+    validationSchema: SignUpSchema,
     onSubmit: async ({ email, password }) => {
-      try {
-        dispatch(signUpThunk({ email, password }));
-      } catch (error) {
-        console.error('Error:', error);
-      }
+      const newUser = { email, password };
+      dispatch(signUpThunk(newUser));
     },
   });
 
@@ -46,43 +51,45 @@ const SignUpForm = () => {
   return (
     <div className={s.container}>
       <div className={s.form}>
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="email">Email Address</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          />
-          {formik.errors.email ? (
-            <div className={s.error}>{formik.errors.email}</div>
-          ) : null}
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            onChange={formik.handleChange}
-            value={formik.values.firstName}
-          />
-          {formik.errors.password ? (
-            <div className={s.error}>{formik.errors.password}</div>
-          ) : null}
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            id="confirmPassword"
-            name="confirmPassword"
-            type="password"
-            onChange={formik.handleChange}
-            value={formik.values.firstName}
-          />
-          {formik.errors.confirmPassword ? (
-            <div className={s.error}>{formik.errors.confirmPassword}</div>
-          ) : null}
+        <FormikContext.Provider value={formik}>
+          <form onSubmit={handleFormSubmit} noValidate>
+            <label htmlFor="email">Email Address</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+            {formik.errors.email ? (
+              <div className={s.error}>{formik.errors.email}</div>
+            ) : null}
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              value={formik.values.firstName}
+            />
+            {formik.errors.password ? (
+              <div className={s.error}>{formik.errors.password}</div>
+            ) : null}
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              onChange={formik.handleChange}
+              value={formik.values.firstName}
+            />
+            {formik.errors.confirmPassword ? (
+              <div className={s.error}>{formik.errors.confirmPassword}</div>
+            ) : null}
 
-          <button type="submit">Submit</button>
-        </form>
+            <button type="submit">Submit</button>
+          </form>
+        </FormikContext.Provider>
       </div>
     </div>
   );
