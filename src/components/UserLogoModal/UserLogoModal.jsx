0dd7e8f235b59385a0498767269cf0ef26843a88
klodmone <kladmone54@gmail.com>
2024-04-20 19:40:23 +0200

    Header

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,8 +1,20 @@
 import css from './UserLogoModal.module.css';
+import sprite from '../../images/sprite.svg';
 const UserLogoModal = () => {
   return (
-    <div className={css.overlay}>
-      <div className={css.modal}>Modal</div>
+    <div className={css.modal}>
+      <button className={css.settingButton}>
+        <svg className={css.toothSvg}>
+          <use href={`${sprite}#icon-6-tooth`}></use>
+        </svg>
+        Setting
+      </button>
+      <button className={css.logOutButton}>
+        <svg className={css.LogOutSvg}>
+          <use href={`${sprite}#icon-arrow-right`}></use>
+        </svg>
+        Log out
+      </button>
     </div>
   );
 };
