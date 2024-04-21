e39c38ce8e7b26e224036b833404b0fd2cb8cd6b
klodmone <kladmone54@gmail.com>
2024-04-21 20:33:23 +0200

    Header

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,6 +1,13 @@
 import css from './UserLogoModal.module.css';
 import sprite from '../../images/sprite.svg';
+import { useState } from 'react';
+import LogOutModal from '../LogOutModal/LogOutModal';
 const UserLogoModal = () => {
+  const [isLogOutModalOpen, setIsLogOutModalOpen] = useState(false);
+
+  const toggleLogOutModal = () => {
+    setIsLogOutModalOpen(!isLogOutModalOpen);
+  };
   return (
     <div className={css.modal}>
       <button className={css.settingButton}>
@@ -9,12 +16,23 @@ const UserLogoModal = () => {
         </svg>
         Setting
       </button>
-      <button className={css.logOutButton}>
+      <button
+        className={css.logOutButton}
+        onClick={() => {
+          toggleLogOutModal();
+        }}
+      >
         <svg className={css.LogOutSvg}>
           <use href={`${sprite}#icon-arrow-right`}></use>
         </svg>
         Log out
       </button>
+      {isLogOutModalOpen && (
+        <LogOutModal
+          isOpen={isLogOutModalOpen}
+          setIsLogOutModalOpen={setIsLogOutModalOpen}
+        />
+      )}
     </div>
   );
 };
