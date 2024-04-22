56f0df36b953590e7450fa13cd02f16ea01c4d03
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2024-04-21 21:29:33 -0500

    fix authform

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,12 +1,8 @@
-
-import SignInForm from '../SignInForm/SignInForm';
-import SignUpForm from '../SignUpForm/SignUpForm';
+import SignInForm from '../components/SignInForm/SignInForm';
+import SignUpForm from '../components/SignUpForm/SignUpForm';
 
 export const AuthForm = ({ isRegister = false }) => {
   return isRegister ? <SignUpForm /> : <SignInForm />;
 };
 
-
-
-SignInPage
-<AuthForm />
+<AuthForm />;
