3301d339fe677900733021f66192cd5ab2526586
Viktoriia3192 <127699121+Viktoriia3192@users.noreply.github.com>
2024-04-22 16:35:57 +0000

    Deploying to gh-pages from @ Viktoriia3192/project-01@8590a4ac40b8483e72dc1091b360bf06d397f0d8 🚀

--- assets/SignUpPage-83489c17.js
+++ assets/SignUpPage-83489c17.js
@@ -1,4 +1,4 @@
-import{b as r,l as a,j as i}from"./index-2c0ed65b.js";import{n as o}from"./emotion-styled.browser.esm-2350cac7.js";import{m as n,a as e,b as p,t as d,c as x,d as l,e as s,f as m,g as u,A as c}from"./AuthForm-4bd28088.js";const g=o.div`
+import{b as r,l as a,j as e}from"./index-b2370803.js";import{n as o}from"./emotion-styled.browser.esm-7530bcf2.js";import{m as p,a as t,b as n,t as d,c as s,d as x,e as l,f as m,g as u,A as g}from"./AuthForm-0256d219.js";const c=o.div`
   max-width: 320px;
   width: 100%;
   height: 570px;
@@ -6,8 +6,8 @@ import{b as r,l as a,j as i}from"./index-2c0ed65b.js";import{n as o}from"./emoti
   top: 150px;
   left: 0;
   z-index: -10;
-  background-image: url(${n}),
-    url(${e});
+  background-image: url(${p}),
+    url(${t});
   background-size: 280px, cover;
   background-repeat: no-repeat;
   background-position:
@@ -18,8 +18,8 @@ import{b as r,l as a,j as i}from"./index-2c0ed65b.js";import{n as o}from"./emoti
     (-webkit-min-device-pixel-ratio: 2),
     (min-resolution: 192dpi),
     (min-resolution: 2dppx) {
-    background-image: url(${p}),
-      url(${e});
+    background-image: url(${n}),
+      url(${t});
   }
 
   @media (min-width: 768px) {
@@ -36,7 +36,7 @@ import{b as r,l as a,j as i}from"./index-2c0ed65b.js";import{n as o}from"./emoti
       (-webkit-min-device-pixel-ratio: 2),
       (min-resolution: 192dpi),
       (min-resolution: 2dppx) {
-      background-image: url(${x});
+      background-image: url(${s});
     }
   }
 
@@ -46,8 +46,8 @@ import{b as r,l as a,j as i}from"./index-2c0ed65b.js";import{n as o}from"./emoti
     height: 100%;
     top: 60px;
     left: 18px;
-    background-image: url(${l}),
-      url(${s});
+    background-image: url(${x}),
+      url(${l});
     background-size: 916px, 1404px;
     background-position:
       top 28px left -51px,
@@ -76,4 +76,4 @@ import{b as r,l as a,j as i}from"./index-2c0ed65b.js";import{n as o}from"./emoti
     margin-left: auto;
     margin-right: 133px;
   }
-`,w=()=>{const t=r(a);return i.jsxs(i.Fragment,{children:[i.jsx(g,{}),i.jsx(c,{}),t&&i.jsx(b,{children:t})]})};export{w as default};
+`,w=()=>{const i=r(a);return e.jsxs(e.Fragment,{children:[e.jsx(g,{isRegister:!0}),e.jsx(c,{}),i&&e.jsx(b,{children:i})]})};export{w as default};
