c9e043c22f064e4c7bc2e5e5d326f89c0f4c0ac5
Viktoriia3192 <vikysja9231@gmail.com>
2024-04-23 20:06:47 +0100

    com2

--- src/components/AddWaterModal/AddWaterModal.jsx
+++ src/components/AddWaterModal/AddWaterModal.jsx
@@ -1,3 +1,125 @@
+
+//   }, [time, waterValue, dispatch]);
+//   const incrementWater = () => {
+//     setWaterValue((prev) => prev + 50);
+//   };
+ 
+// const handleFormSubmit = (event) => {
+//   event.preventDefault();
+
+//   const additionalWaterVolume = parseFloat(event.currentTarget.water.value);
+
+//   const formData = {
+//     additionalWaterVolume: additionalWaterVolume,
+//   };
+
+//   handleAddWater(formData);
+
+//   onClose();
+
+//   event.currentTarget.reset();
+// };
+
+
+//   const decrementWater = () => {
+//     setWaterValue((prev) => (prev > 0 ? prev - 50 : 0));
+//   };
+
+//   const handleTimeChange = (event) => {
+//     setTime(event.target.value);
+//   };
+
+//   const generateOptions = () => {
+//     let options = [];
+//     for (let hour = 0; hour < 24; hour++) {
+//       for (let minute = 0; minute < 60; minute += 15) {
+//         let formattedHour = hour.toString().padStart(2, '0');
+//         let formattedMinute = minute.toString().padStart(2, '0');
+//         options.push(`${formattedHour}:${formattedMinute}`);
+//       }
+//     }
+//     return options;
+//   };
+
+//   const handleSave = async () => {
+//     const currentDate = new Date();
+//     const year = currentDate.getFullYear();
+//     const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+//     const day = String(currentDate.getDate()).padStart(2, '0');
+
+//     const formattedDate = `${year}-${month}-${day}`;
+
+//     const newNote = {
+//       waterVolume: waterValue,
+//       date: formattedDate,
+//     };
+//     await dispatch(addWaterThunk(newNote));
+//     // setWaterValue(newNote.waterVolume);
+//     // setTime();
+//     onClose();
+//   };
+
+//   return (
+//     <>
+//       <div className={css.add_water_modal}>
+//         <h2 className={css.title}>Add Water</h2>
+//         <p className={css.value}>Choose a value:</p>
+//         <p className={css.title2}>Amount of water:</p>
+//         <div className={css.count_water}>
+//           <button className={css.count_button} onClick={decrementWater}>
+//             <svg className={css.count_icon}>
+//               <use xlinkHref={`${sprite}#icon-minus-small`} />
+//             </svg>
+//           </button>
+//           <p className={css.amount_water}>{waterValue} ml</p>
+//           <button className={css.count_button} onClick={incrementWater}>
+//             <GrAdd className={css.plus_icon} />
+//           </button>
+//         </div>
+//         <p className={css.recording_time}>Recording time:</p>
+
+
+//         <div className={css.time_selection}>
+//           <form name="water_value" onSubmit={handleFormSubmit}>
+//           <select
+//             className={css.custom_select}
+//             name="time"
+//             value={time}
+//             onChange={handleTimeChange}
+//           >
+//             {generateOptions().map((option) => (
+//               <option key={option} value={option}>
+//                 {option}
+//               </option>
+//             ))}
+//           </select>
+//         </div>
+//         <p className={css.water_value_title}>
+//             Enter the value of the water used:
+//           </p>
+
+//           <input
+//             className={css.water_value}
+//             type="number"
+//             name="water"
+//             min="0"
+//             value={waterValue}
+//             onChange={(e) => setWaterValue(e.target.value)}
+//           />
+//         <div className={css.water_value_button_send}>
+//             <p className={css.water}>{waterValue}ml</p>
+//             <button className={css.button_save} type="submit">
+//               Save
+//             </button>
+//           </div>
+        
+//       </div>
+//     </>
+//   );
+// }
+
+
+
 import { useEffect, useState } from 'react';
 import sprite from '../../images/sprite.svg';
 import css from './AddWater.module.css';
@@ -14,11 +136,11 @@ export default function AddWaterModal({ onClose }) {
   const [time, setTime] = useState('00:00');
   const [waterValue, setWaterValue] = useState(250);
   const dispatch = useDispatch();
-  const { percentageWaterDrunk } = useSelector(selectTodayWater);
 
   useEffect(() => {
     dispatch(todayThunk());
-  }, [time, waterValue, dispatch]);
+  }, [dispatch]);
+
   const incrementWater = () => {
     setWaterValue((prev) => prev + 50);
   };
@@ -43,21 +165,18 @@ export default function AddWaterModal({ onClose }) {
     return options;
   };
 
-  const handleSave = async () => {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
 
-    const formattedDate = `${year}-${month}-${day}`;
+    const waterVolume = parseFloat(event.currentTarget.water.value);
 
     const newNote = {
-      waterVolume: waterValue,
-      date: formattedDate,
+      waterVolume,
+      date: new Date().toISOString(),
     };
-    await dispatch(addWaterThunk(newNote));
-    // setWaterValue(newNote.waterVolume);
-    // setTime();
+
+    dispatch(addWaterThunk(newNote));
+
     onClose();
   };
 
@@ -79,9 +198,7 @@ export default function AddWaterModal({ onClose }) {
           </button>
         </div>
         <p className={css.recording_time}>Recording time:</p>
-
-
-        <div className={css.time_selection}>
+        <form name="water_value" onSubmit={handleFormSubmit}>
           <select
             className={css.custom_select}
             name="time"
@@ -94,11 +211,9 @@ export default function AddWaterModal({ onClose }) {
               </option>
             ))}
           </select>
-        </div>
-        <p className={css.water_value_title}>
+          <p className={css.water_value_title}>
             Enter the value of the water used:
           </p>
-
           <input
             className={css.water_value}
             type="number"
@@ -107,13 +222,13 @@ export default function AddWaterModal({ onClose }) {
             value={waterValue}
             onChange={(e) => setWaterValue(e.target.value)}
           />
-        <div className={css.water_value_button_send}>
-            <p className={css.water}>{waterValue}ml</p>
+          <div className={css.water_value_button_send}>
+            <p className={css.water}>{waterValue} ml</p>
             <button className={css.button_save} type="submit">
               Save
             </button>
           </div>
-        
+        </form>
       </div>
     </>
   );
