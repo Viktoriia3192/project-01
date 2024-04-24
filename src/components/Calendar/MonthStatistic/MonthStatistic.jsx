8273379e9079841970966e4cf00205d325b637c6
Viktoriia3192 <vikysja9231@gmail.com>
2024-04-24 16:32:22 +0100

    memo

--- src/components/Calendar/MonthStatistic/MonthStatistic.jsx
+++ src/components/Calendar/MonthStatistic/MonthStatistic.jsx
@@ -1,22 +1,24 @@
-import s from './MonthStatistic.module.css'
+import s from './MonthStatistic.module.css';
 import { compareDates, today } from '../helpers/getDate';
 import { getMonthsArr } from '../helpers/getMonthsArr';
 import PopoverDay from '../DaysGeneralStats/PopoverDay';
 import { useSelector } from 'react-redux';
-import { selectTodayWater } from '../../../redux/waterData/waterSelectors'
+import { selectTodayWater } from '../../../redux/waterData/waterSelectors';
 
 const MonthStatistic = ({ selectedMonth, monthStatistic, registrationDate }) => {
-  const { percentageWaterDrunk} = useSelector(selectTodayWater);
+  const { percentageWaterDrunk } = useSelector(selectTodayWater);
+
   const currentMonth = (year, month, statistic) => {
     const daysArr = [];
     const monthData = getMonthsArr(year)[month];
+
     if (!monthData) {
       return daysArr;
     }
+
     const picData = [selectedMonth.month, selectedMonth.year];
     const todayData = [today.month, today.year];
     const compare = compareDates(picData, todayData);
-
 
     for (let i = 1; i <= monthData.numberOfDays; i += 1) {
       const day = statistic.find(statisticOneDay => {
@@ -28,10 +30,10 @@ const MonthStatistic = ({ selectedMonth, monthStatistic, registrationDate }) => 
 
       if (compare === 0) {
         if (!day && i <= today.day) {
-          daysArr.push({ date: i, percent: '0%', norm: '2L', drinks: 0 });
+          daysArr.push({ date: i, percent: '0%', norm: '2L', drinks: 0, percentageWaterDrunk });
         }
         if (!day && i > today.day) {
-          daysArr.push({ date: i, percent: '' });
+          daysArr.push({ date: i, percent: '', percentageWaterDrunk });
         }
         if (day) {
           const drinkCount = Array.isArray(day.drinks)
@@ -42,27 +44,28 @@ const MonthStatistic = ({ selectedMonth, monthStatistic, registrationDate }) => 
             percent: `${day.percent}%`,
             norm: `${day.norm / 1000}L`,
             drinks: drinkCount,
+            percentageWaterDrunk,
           });
         }
       }
       if (compare === 1) {
-        daysArr.push({ date: i, percent: '' });
+        daysArr.push({ date: i, percent: '', percentageWaterDrunk });
       }
 
       if (compare === -1) {
         if (day) {
           daysArr.push({
             date: i,
-            percent:`${day.percent}%`,
+            percent: `${day.percent}%`,
             norm: `${day.norm / 1000}L`,
             drinks: day.drinks,
+            percentageWaterDrunk,
           });
-        } else if (selectedMonth.year === registrationDate.year && selectedMonth.month === registrationDate.month && i < registrationDate.day) { 
-          daysArr.push({ date: i, percent: '', norm: '2L', drinks: 0 });
+        } else if (selectedMonth.year === registrationDate.year && selectedMonth.month === registrationDate.month && i < registrationDate.day) {
+          daysArr.push({ date: i, percent: '', norm: '2L', drinks: 0, percentageWaterDrunk });
 
-        }
-        else {
-          daysArr.push({ date: i, percent: '0%', norm: '2L', drinks: 0 });
+        } else {
+          daysArr.push({ date: i, percent: '0%', norm: '2L', drinks: 0, percentageWaterDrunk });
         }
       }
     }
@@ -78,15 +81,15 @@ const MonthStatistic = ({ selectedMonth, monthStatistic, registrationDate }) => 
   return (
     <>
       <div className={s.div}>
-        {statistic.map(({ date, drinks, norm }) => (
+        {statistic.map(({ date, drinks, norm, percentageWaterDrunk }) => (
           <li className={s.li} key={date}>
             <PopoverDay
               key={date}
               date={date}
               drinks={drinks}
               norm={norm}
-      
               selectedMonth={selectedMonth}
+              percentageWaterDrunk={percentageWaterDrunk}
             />
             <p className={s.p}>{percentageWaterDrunk}%</p>
           </li>
