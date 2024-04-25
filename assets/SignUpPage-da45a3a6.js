fbd7b1b7006febddd9b4bd347e56b89171868a10
Viktoriia3192 <127699121+Viktoriia3192@users.noreply.github.com>
2024-04-25 01:25:54 +0000

    Deploying to gh-pages from @ Viktoriia3192/project-01@3054f2561456342c6999a637dd4c4e54db707aac 🚀

--- assets/SignUpPage-da45a3a6.js
+++
@@ -1,1 +0,0 @@
-import{b as e,m as t,j as r}from"./index-b4e9e2f2.js";import{S as o,h as a,A as i,i as n}from"./SignUpPage.styled-2e3e5d7a.js";import"./emotion-styled.browser.esm-32af96fc.js";const c=()=>{const s=e(t);return r.jsxs(o,{children:[r.jsx(a,{}),r.jsx(i,{isRegister:!0}),s&&r.jsx(n,{children:s})]})};export{c as default};
